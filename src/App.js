a0fa9d1cf4ee1dca36de49b061c27719054aff24
codefromhome2011 <62923420+codefromhome2011@users.noreply.github.com>
2022-04-19 14:26:04 +0700

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const courses = [
+  {
+    id : 1,
+    name : 'JavaScript'
+  },
+  {
+    id : 2,
+    name : 'ReactJS'
+  },
+  {
+    id : 3,
+    name : 'NodeJS'
+  }
+]
 
 function App() {
+  const   [checked1, setChecked1]  = useState(1)
+  
+  const   [checked2, setChecked2]  = useState([])
+
+  const handleSubmitRadio = () => {
+    console.log(checked1)
+    alert(`You have registered course ${JSON.stringify(checked1)}`)
+  }
+  
+  const handleSubmitCheckBox = () => {
+    console.log(checked2)
+    alert(`You have registered course ${JSON.stringify(checked2)}`)
+  }
+  
+  const handleCheck = (id) => {
+    setChecked2(prev => {
+      const isChecked = checked2.includes(id)
+      if (isChecked) {
+        return checked2.filter(item => item !== id)
+      } else {
+        return [...prev,id]
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding : 32}}>
+      {courses.map(course => (
+          <div key = {course.id}>
+            <input 
+              type = 'radio' 
+              checked = {checked1 === course.id}
+              onChange={() => setChecked1(course.id)}
+            />
+            {course.name}
+          </div>
+        )) }
+      <button onClick={handleSubmitRadio}>Register</button>
+      
+      {courses.map(course => (
+          <div key = {course.id}>
+            <input 
+              type = 'checkbox' 
+              checked = {checked2.includes(course.id)}
+              onChange={() => handleCheck(course.id)}
+            />
+            {course.name}
+          </div>
+        ))}
+      <button onClick={handleSubmitCheckBox}>Register</button>
     </div>
   );
 }
